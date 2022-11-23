6c3eb66501ea0ee294627e414b1212da8cf6d930
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-11-23 17:31:27 -0300

    feat: cardTotal styled component

--- src/components/CartTotal/index.jsx
+++ src/components/CartTotal/index.jsx
@@ -1,13 +1,16 @@
-import "./style.css";
+import { StyledCartTotal } from "./style";
+// import "./style.css";
 
 export const CartTotal = () => {
   return (
-    <div>
+    <StyledCartTotal>
+      {/* <div> */}
       <div>
         <h3 className="font-heading-4">Total</h3>
         <span className="font-body-600-gray">R$ 40,00</span>
       </div>
       <button className="btn-default btn-gray">Remover Todos</button>
-    </div>
+      {/* </div> */}
+    </StyledCartTotal>
   );
 };
